397a509a79f7e1a4c02a2bab21a370f5f1e528e9
Melven <melvenbagolaque@gmail.com>
2023-09-11 21:51:14 +0800

    WP-001 feature: added utils folder and navlinks

---
+++ src/utils/navlinks.jsx
@@ -0,0 +1,17 @@
+export const navlink = [
+    {
+        id: 1,
+        name: 'Home',
+        link: 'Home'
+    },
+    {
+        id: 2,
+        name: 'About',
+        link: 'About'
+    },
+    {
+        id: 3,
+        name: 'Rent',
+        link: 'Rent'
+    },
+]
