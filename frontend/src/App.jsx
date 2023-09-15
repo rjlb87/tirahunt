530ff5e2758a36cd525aa2b866d3f259c6ae5dd7
Melven <melvenbagolaque@gmail.com>
2023-09-16 02:56:41 +0800

    feature: added functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import LogIn from "./authentication/admin/LogIn";
 import Footer from "./common/Footer";
 import NavBar from "./common/NavBar";
 import Home from "./components/Home";
+import Signup from "./authentication/admin/SignUp";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <Signup />
       <LogIn />
       <Footer />
     </>
