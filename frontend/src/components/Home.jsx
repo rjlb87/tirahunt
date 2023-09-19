da462703c7c8faa3eb91d346ce9a19336d348275
Melven <melvenbagolaque@gmail.com>
2023-09-20 01:29:12 +0800

    added Home components and services for properties

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,47 +1,58 @@
-import React from "react";
-import { cards } from "../utils/cards";
+import React, { useEffect, useState } from "react";
+import { getAllProperties } from "../services/PropertyListings";
 
 function Home() {
+  const [properties, setProperties] = useState([]);
+
+  const formatPrice = (price) => {
+    const formattedPrice = price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    return `₱${formattedPrice}`;
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getAllProperties();
+        setProperties(data);
+      } catch (error) {
+        console.error("Error", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="max-w-6xl mx-auto h-fit mt-10">
-      <div className="flex flex-col justify-center ">
-        <ul className="grid grid-cols-4">
-          {cards.map(
-            ({
-              id,
-              description,
-              location,
-              price,
-              image,
-              person,
-              bedrooms,
-              bathRoom,
-              livingRoom,
-              rating,
-            }) => (
-              <li
-                className="ml-4 border-2 shadow-md shadow-gray-500 mb-8 border-gray-100 rounded-md"
-                key={id}
-              >
-                <li>
-                  <img className="rounded-lg" src={image} alt="image" />
-                </li>
-                <div className="px-4 py-4">
-                  <li className="font-semibold text-lg">
-                    {location}
-                    <span className="ml-20">{rating}</span>
-                  </li>
-                  <li className="text-gray-300">{description}</li>
-                  <li>{person}</li>
-                  <li>{bedrooms}</li>
-                  <li>{bathRoom}</li>
-                  <li>{livingRoom}</li>
-                  <li className="font-semibold text-md ">{price}</li>
-                </div>
-              </li>
-            )
-          )}
-        </ul>
+    <div className="h-screen max-w-6xl mx-auto mt-10">
+      <h1 className="text-3xl font-semibold mb-4">Property List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {properties.map((property) => (
+          <div
+            key={property.property_id}
+            className="bg-white p-4 rounded shadow"
+          >
+            <img
+              src={'./public/img_1.jpg'}
+              alt="Property"
+              className="w-full h-auto"
+            />
+            <h2 className="text-xl font-semibold">{property.description}</h2>
+            <p className="text-gray-600">Location: {property.location}</p>
+            <p className="text-gray-600">
+              Price: {formatPrice(parseFloat(property.price))}
+            </p>
+            <p className="text-gray-600">Bedrooms: {property.bedrooms}</p>
+            <p className="text-gray-600">Bathrooms: {property.bathrooms}</p>
+            <p className="text-gray-600">
+              Living Rooms: {property.living_rooms}
+            </p>
+            <p className="text-gray-600">Rating: {property.rating}</p>
+            <p className="text-gray-600">
+              Property Type: {property.property_type}
+            </p>
+            <p className="text-gray-600">Username: {property.users.username}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ frontend/src/services/PropertyListings.jsx
@@ -0,0 +1,33 @@
+export async function getAllProperties() {
+  try {
+    const response = await fetch("/api/v1/listings");
+    console.log("here", response);
+    return await response.json();
+  } catch (error) {
+    console.error(error.message);
+    throw Error;
+  }
+}
+
+export const createProperty = async (property, selectedImage, user_id) => {
+  try {
+    property.image_id = selectedImage;
+    property.user_id = user_id;
+    const response = await fetch("/api/v1/listings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(property),
+    });
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Error creating property");
+    }
+  } catch (error) {
+    console.error("Error creating property", error);
+    throw error;
+  }
+};
