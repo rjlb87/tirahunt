c7c96f210f7720278df3d14786d85333726253e0
Melven <melvenbagolaque@gmail.com>
2023-10-12 01:12:19 +0800

    feature: added filter for property type

--- frontend/src/common/Routes.jsx
+++ frontend/src/common/Routes.jsx
@@ -1,13 +1,13 @@
 import NavBar from "./NavBar";
 import Home from "../components/Home";
-import Category from "../components/Category";
+// import Category from "../components/Category";
 import Footer from "../common/Footer";
 
 function Routes() {
   return (
     <>
       <NavBar />
-      <Category />
+      {/* <Category /> */}
       <Home />
       <Footer />
     </>
